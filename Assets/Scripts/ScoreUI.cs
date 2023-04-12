0e99a5d4e707058bc60271ef553d2d7f72567e2f
JVdeOliveira <joaooliveira.jvo@outlook.com>
2023-04-12 18:03:11 -0300

    -ScoreUI update
    - Changing names

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -1,28 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class ScoreUI : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI TMP_Player1;
-    [SerializeField] private TextMeshProUGUI TMP_Player2;
+    [SerializeField] private TextMeshProUGUI textScorePlayer1;
+    [SerializeField] private TextMeshProUGUI textScorePlayer2;
 
     private void Start()
     {
-        GameController.Instance.OnScoredChanded += GameController_OnScoredChanded;
+        GameController.Instance.ScoredChanged += GameController_ScoredChanded;
     }
 
-    private void GameController_OnScoredChanded(object sender, GameController.ScoredChandedEventArgs e)
+    private void GameController_ScoredChanded(object sender, GameController.ScoredChangedEventArgs e)
     {
         switch (e.sideThatScored)
         {
             case Side.Left:
-                TMP_Player1.text = e.score.ToString();
+                textScorePlayer1.text = e.score.ToString();
                 break;
 
             case Side.Right:
-                TMP_Player2.text= e.score.ToString();
+                textScorePlayer2.text= e.score.ToString();
                 break;
         }
     }
